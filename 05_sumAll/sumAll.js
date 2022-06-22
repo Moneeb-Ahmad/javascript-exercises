2eb5fe95b00f56ee80816401bc18d4e1599a1d72
Moneeb-Ahmad <moneebahmad32@gmail.com>
2022-06-22 10:12:16 -0400

    05_sumAll done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+  if(!(Number.isInteger(num1) && Number.isInteger(num2))) {
+    return "ERROR";
+  }
+  else if(num1 < 0 || num2 < 0) {
+    return "ERROR";
+  }
+  let left = num1 < num2 ? num1 : num2;
+  let right = num1 < num2 ? num2 : num1;
+  let ret = 0;
+  for(let i = left; i <= right; i++) {
+    ret += i;
+  }
+  return ret;
 };
 
 // Do not edit below this line
