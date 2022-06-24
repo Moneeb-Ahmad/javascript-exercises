6a75f17d9853c1c648694adf325f730c390c4957
Moneeb-Ahmad <moneebahmad32@gmail.com>
2022-06-24 11:36:27 -0400

    10_fibonacci done

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+  n = Number(n);
+  let arr = [];
+  if (n < 0) return "OOPS"
+  for (let i = 0; i < n; i++) {
+    if (i == 0 || i == 1) {
+      arr.push(1);
+      continue;
+    } else {
+      let val = arr[i - 1] + arr[i - 2];
+      arr.push(val);
+    }
+  }
+  return arr[n - 1];
 };
 
 // Do not edit below this line
