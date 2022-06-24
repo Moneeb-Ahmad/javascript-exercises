759e4c5bd7c513525c80e69163d31e2749e292bc
Moneeb-Ahmad <moneebahmad32@gmail.com>
2022-06-24 11:16:37 -0400

    09_palindromes done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,30 @@
-const palindromes = function () {
-
+const palindromes = function (s) {
+  const regex = /^[a-z0-9]+$/i;
+  s = s.trim();
+  s = s.toLowerCase();
+  if(s === "") {
+    return true;
+  }
+  let left = 0;
+  let right = s.length-1;
+  while(!(left >= right)) {
+    if(s.charAt(left) === s.charAt(right) ||
+      !s.charAt(left).match(regex) ||
+      !s.charAt(right).match(regex)) {
+        oldLeft = left;
+        oldRight = right;
+        left = !s.charAt(left).match(regex) ? left + 1 : left;
+        right = !s.charAt(right).match(regex) ? right - 1 : right;
+        if(oldLeft === left && oldRight === right) {
+          left++;
+          right--;
+        }
+      }
+      else {
+        return false;
+      }
+  }
+  return true;
 };
 
 // Do not edit below this line
