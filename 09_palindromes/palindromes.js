d3bd66b2421cee38a8d8c9abc412d6a73d1077c0
Moneeb-Ahmad <moneebahmad32@gmail.com>
2022-06-24 11:17:12 -0400

    09_palindromes formatting

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,28 +1,27 @@
-const palindromes = function (s) {
+const palindromes = function(s) {
   const regex = /^[a-z0-9]+$/i;
   s = s.trim();
   s = s.toLowerCase();
-  if(s === "") {
+  if (s === "") {
     return true;
   }
   let left = 0;
-  let right = s.length-1;
-  while(!(left >= right)) {
-    if(s.charAt(left) === s.charAt(right) ||
+  let right = s.length - 1;
+  while (!(left >= right)) {
+    if (s.charAt(left) === s.charAt(right) ||
       !s.charAt(left).match(regex) ||
       !s.charAt(right).match(regex)) {
-        oldLeft = left;
-        oldRight = right;
-        left = !s.charAt(left).match(regex) ? left + 1 : left;
-        right = !s.charAt(right).match(regex) ? right - 1 : right;
-        if(oldLeft === left && oldRight === right) {
-          left++;
-          right--;
-        }
+      oldLeft = left;
+      oldRight = right;
+      left = !s.charAt(left).match(regex) ? left + 1 : left;
+      right = !s.charAt(right).match(regex) ? right - 1 : right;
+      if (oldLeft === left && oldRight === right) {
+        left++;
+        right--;
       }
-      else {
-        return false;
-      }
+    } else {
+      return false;
+    }
   }
   return true;
 };
