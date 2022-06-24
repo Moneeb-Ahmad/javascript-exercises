9af38597b46e4975cff9360da16260f2922f38e0
Moneeb-Ahmad <moneebahmad32@gmail.com>
2022-06-24 12:03:39 -0400

    12_findTheOldest done

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+  let oldest = arr.reduce((lastGuy, nextGuy) => {
+    let key = 'yearOfDeath';
+    if (!(key in lastGuy)) {
+      lastGuy[key] = (new Date()).getFullYear();
+    }
+    if (!(key in nextGuy)) {
+      nextGuy[key] = (new Date()).getFullYear();
+    }
+    if ((lastGuy.yearOfDeath - lastGuy.yearOfBirth) >
+      (nextGuy.yearOfDeath - nextGuy.yearOfBirth)) {
+      return lastGuy;
+    } else {
+      return nextGuy;
+    }
+  });
+  return oldest;
 };
 
 // Do not edit below this line
