e9950b99997a5d7f8c0869a5578e9fe8d8cb80fa
Moneeb-Ahmad <moneebahmad32@gmail.com>
2022-06-24 11:02:12 -0400

    08_calculator done

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,36 @@
-const add = function() {
-	
+const add = function(n1, n2) {
+  return n1 + n2;
 };
 
-const subtract = function() {
-	
+const subtract = function(n1, n2) {
+  return n1 - n2;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  const ret = arr.reduce((total, value) => {
+    return total + value;
+  }, 0)
+  return ret;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  const ret = arr.reduce((total, value) => {
+    return total * value;
+  }, 1)
+  return ret;
 };
 
-const power = function() {
-	
+const power = function(n1, n2) {
+  return n1 ** n2;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  let ret = 1;
+  while (n > 1) {
+    ret *= n;
+    n -= 1;
+  }
+  return ret;
 };
 
 // Do not edit below this line
